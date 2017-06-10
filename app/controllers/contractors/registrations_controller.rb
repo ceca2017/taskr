adbceee0dd4bfc8ac4b38b0d66b913ce014f6ee6
claes <claesca@outlook.com>
2017-06-10 20:01:53 +0200

    started update the edit form for contractor - now possible to update the company name

--- app/controllers/contractors/registrations_controller.rb
+++ app/controllers/contractors/registrations_controller.rb
@@ -9,5 +9,10 @@ class Contractors::RegistrationsController < Devise::RegistrationsController
                :remember_me, :address, :city, :state, :zip_code, :country,
                :company_name, :experience)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:username, :first_name, :last_name, :email, :password, :address,
+               :remember_me, :address, :city, :state, :zip_code, :country,
+               :company_name, :experience, :current_password)
+    end
   end
 end
