c673fa4d85a3e0a1c9d059f98d7ac071a46ec6a2
claes <claesca@outlook.com>
2017-06-12 09:56:32 +0200

    update the task controller to send only the task id as params

--- app/controllers/requesters/tasks_controller.rb
+++ app/controllers/requesters/tasks_controller.rb
@@ -4,13 +4,7 @@ class Requesters::TasksController < ApplicationController
   end
 
   def show
-    if params[:format].nil?
-      # the requester has clicked on a link to see details on a specific task
-      @task = Task.find(params[:id])
-    else
-      # the reqester has just created a new task
-      @task = Task.find(params[:format])
-    end
+    @task = Task.find(params[:id])
     @task_cat = TaskCategory.find(@task.task_category_id)
   end
 end
